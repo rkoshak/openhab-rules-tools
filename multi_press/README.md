7c7f02281957383bf409b5212b9873c63e791281
Sascha Volkenandt <sascha@akv-soft.de>
2020-09-23 15:49:15 +0200

    add docstrings and README.md

---
+++ multi_press/README.md
@@ -0,0 +1,70 @@
+# Multi Press Profile
+This script registers as a profile that can be used to track button events from a trigger channel and sends commands representing the number of consecutive taps or a hold / release event to the linked item.
+
+# Purpose
+Sometimes it is desirable to fully customize what should happen if a button (momentary switch) attached to a thing is pressed.
+For example the button of a Shelly Dimmer device can either control only the output of the same device (no special functions) or send events and not control anything at all. In that second case, OpenHAB has to take care of controlling both the native functions of the device and any special functions.
+
+# Requirements
+None
+
+# How it works
+The script will register a profile to be used when linking a trigger channel delivering button events to an item receiving commands representing the number of consecutive taps or a hold / release event.
+Since profiles are instantiated when an item using the profile is loaded there is no need to trigger reloading of the profile manually (no MultiPress_Reload item is created).
+
+The profile is attached via profile metadata when linking a channel to an item.
+
+```
+channel="CHANNEL"[profile="jython:multiPress", on="ON", off="OFF", shortDelay=200, longDelay=1000]
+```
+
+Argument | Values | Purpose
+-|-|-
+`"CHANNEL"` | UID of the channel | The channel that triggers upon pressing or releasing the button.
+`on` | Press event | String the channel triggers when the button is pressed. Defaults to `ON`
+`off` | Release event | String the channel triggers when the button is released. Defaults to `OFF`
+`shortDelay` | Short tap delay | The time in milliseconds to wait for another press after the button has been released. Defaults to `200`
+`longDelay` | Long tap delay | The time in milliseconds the button has to be pressed before reporting a hold event. Defaults to `1000`
+
+The item will receive the following commands.
+
+Command | Purpose
+-|-
+`1` | The button has been pressed and released within `longDelay` milliseconds once.
+`2`..`N` | The button has been tapped N times with less than `shortDelay` milliseconds between taps.
+`HOLD` | The button has been held pressed for longer than `longDelay` milliseconds.
+`RELEASE` | The button has been released after a `HOLD` command has been issued.
+
+When encountering a `HOLD` event previous taps are discarded, so if you tap-tap-hold the button, only `HOLD` is reported.
+
+When defining the item, `autoupdate=false` might be desirable to the item only received commands but never updates, unless you would like to have the last event reflected in the item state.
+
+# Examples
+```
+// Channel triggers ON and OFF, default timeouts apply
+Item MyButton { channel="mqtt:topic:mosquitto:MyDevice:Input"[profile="jython:multiPress"], autoupdate=false }
+
+// Channel triggers ON and OFF, default timeouts apply, item state doesn't get updated
+Item MyButton { channel="mqtt:topic:mosquitto:MyDevice:Input"[profile="jython:multiPress"], autoupdate=false }
+
+// Channel triggers 0 and 1, custom timeouts
+Item MyButton { channel="mqtt:topic:mosquitto:MyDevice:Input"[profile="jython:multiPress", on="1", off="0", shortDelay=250, longDelay=2000] }
+
+// Example rule
+rule "MyButton functions"
+when
+    Item MyButton received command
+then
+    if (receivedCommand == "1") {
+        MyCeilingLamp.sendCommand(ON if MyCeilingLamp.state == OFF else OFF)
+    } else if (receivedCommand == "2") {
+        MyReadingLamp.sendCommand(ON if MyReadingLamp.state == OFF else OFF)
+    } else if (receivedCommand == "HOLD") {
+        MyCeilingLamp.sendCommand(OFF)
+        MyReadingLamp.sendCommand(OFF)
+    }
+end
+```
+
+# Limitations
+- Does not handle complex patterns (i.e. tap-tap-hold is only reported as `HOLD`)
--- multi_press/automation/jsr223/python/community/multi_press/multi_press.py
+++ multi_press/automation/jsr223/python/community/multi_press/multi_press.py
@@ -1,3 +1,18 @@
+"""
+Copyright September 23, 2020 Sascha Volkenandt
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 scriptExtension.importPreset(None) # fix for compatibility with Jython > 2.7.0
 
 import core
@@ -16,8 +31,16 @@ FACTORY_CLASS = "{}.{}".format(ProfileFactory.__module__, ProfileFactory.__name_
 UID_MULTI_PRESS = ProfileTypeUID("jython", "multiPress")
 
 class MultiPressProfile(TriggerProfile):
+    """
+    The profile class is instantiated when a link using this profile gets triggered for the first 
+    time. Holds configuration, callback and internal state of the profile.
+    """
 
     def __init__(self, callback, context):
+        """
+        Constructor
+        """
+
         log.info("Initializing MultiPressProfile with configuration {}".format(context.configuration))
         self.callback = callback
         self.context = context
@@ -26,6 +49,11 @@ class MultiPressProfile(TriggerProfile):
         self.clicks = 0
 
     def onTriggerFromHandler(self, event):
+        """
+        Overrides TriggerProfile#onTriggerFromHandler
+        Gets called every time the channel triggers and tracks the number of consecutive taps.
+        """
+
         if self.__stateChanged(event):
             self.__cancel()
 
@@ -39,19 +67,36 @@ class MultiPressProfile(TriggerProfile):
                 log.debug("Arming {} ms timer for multiPress profile".format(delay))
                 self.clicks += 1
                 self.future = self.context.executorService.schedule(
-                            lambda: self.__clicks(), delay, TimeUnit.MILLISECONDS)
-                else:
-                    self.clicks = 0
+                        lambda: self.__clicks(), delay, TimeUnit.MILLISECONDS)
+            else:
+                self.clicks = 0
 
-        def onStateUpdateFromItem(self, state):
-            pass # channel is supposed to be read-only
+    def onStateUpdateFromItem(self, state):
+        """
+        Overrides TriggerProfile#onStateUpdateFromItem
+        Gets called every time the item updates its state. Ignored since channel is supposed
+        to be read-only.
+        """
 
-        def __cancel(self):
-            if not self.future is None:
+        pass
+
+    def __cancel(self):
+        """
+        Cancels any previously scheduled timer.
+        """
+
+        if not self.future is None:
             self.future.cancel(True)
             self.future = None
 
     def __stateChanged(self, event):
+        """
+        Translates the trigger event into a boolean representing the current state and returns
+        if the state has changed since the last invocation. This allows for devices that 
+        occasionally report an event although the button has not been touched (i.e. Shelly 
+        Dimmer).
+        """
+
         onValue = str(self.context.configuration.get("on") or "ON")
         offValue = str(self.context.configuration.get("off") or "OFF")
         if event == onValue:
@@ -69,21 +114,42 @@ class MultiPressProfile(TriggerProfile):
         return False
 
     def __longPress(self):
+        """
+        Gets invoked by a timer firing after longDelay ms and reports a HOLD event to the item.
+        """
+
         log.debug("Detected long press on multiPress profile")
         self.callback.sendCommand(StringType("HOLD"))
         self.clicks = -1
 
     def __clicks(self):
+        """
+        Gets invoked by a timer firing after shortDelay ms and reports the number of taps 
+        encountered consecutively.
+        """
+
         log.debug("Detected {} clicks on multiPress profile".format(self.clicks))
         self.callback.sendCommand(StringType(str(self.clicks)))
         self.clicks = 0
 
 class MultiPressProfileFactory(ProfileFactory):
+    """
+    The profile factory class gets injected into OpenHABs service registry and can thus be used 
+    by specifying "jython:multiPress" as a profile when linking channels and items.
+    """
 
     def createProfile(self, type, callback, context):
+        """
+        Called when a link using this profile gets triggered for the first time.
+        """
+
         return MultiPressProfile(callback, context)
 
     def getSupportedProfileTypeUIDs(self):
+        """
+        Reports, which profiles this factory supports to OpenHAB's service registry.
+        """
+
         return [UID_MULTI_PRESS]
 
 try:        
@@ -93,11 +159,19 @@ except:
     log.error(traceback.format_exc())
 
 def scriptLoaded(*args):
+    """
+    Registers the profile factory in OpenHAB's service registry.
+    """
+
     if core.MultiPressProfileFactory is not None:
         register_service(core.MultiPressProfileFactory, [FACTORY_CLASS])
         log.debug("Registered service")
 
 def scriptUnloaded():
+    """
+    Removes the profile factory from OpenHAB's service registry.
+    """
+
     if core.MultiPressProfileFactory is not None:
         unregister_service(core.MultiPressProfileFactory)
         core.MultiPressProfileFactory = None
