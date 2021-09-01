a87f1f496c42c02780165e90f72adb8c827a3b2e
Florian Hotze <florianh_dev@icloud.com>
2021-09-01 12:00:05 +0200

    Update groupUtils.js
    * Changed from class to stand-alone functions.
    * Now using map/reduce for stream in getMembers and getAllMembers.

--- group_utils/javascript/automation/lib/javascript/community/groupUtils.js
+++ group_utils/javascript/automation/lib/javascript/community/groupUtils.js
@@ -2,137 +2,140 @@
  * Utilities for working with openHAB groups in JavaScript.
  * This code is not compatible with the GraalVM JavaScript add-on.
  *
- * Tested to work with openHAB 3.1.0 stable.
  * Copyright (c) 2021 Florian Hotze under MIT License
  * @florian-h05(https://github.com/florian-h05)
  */
 
-/**
- * Constructor, initializes the logger.
- */
-var GroupUtils = function () {
-  this.log = Java.type('org.slf4j.LoggerFactory').getLogger('org.openhab.model.script.Rules.GroupUtils')
-  this.log.debug('Building groupUtils instance.')
-}
+(function (context) {
+  /**
+   * Imports.
+   */
+  var logger = Java.type('org.slf4j.LoggerFactory').getLogger('org.openhab.model.script.Rules.GroupUtils')
+  var Collectors = Java.type('java.util.stream.Collectors')
 
-/**
- * Get the direct members of a group.
- *
- * @param {*} group Name of the openHAB group
- * @param {*} characteristic Which characteristic of the members you get, valid: name (default), label, state
- */
-GroupUtils.prototype.getMembers = function (group, characteristic) {
-  this.log.debug('Getting direct members of group ' + group)
-  var groupMembers = Array()
-  itemRegistry.getItem(group).getMembers().stream().forEach(function (item) {
-    if (characteristic === 'label') {
-      groupMembers.push(item.getLabel())
-    } else if (characteristic === 'state') {
-      groupMembers.push(item.getState())
-    } else {
-      groupMembers.push(item.getName())
-    }
-  })
-  // return the array
-  return groupMembers
-}
-
-/**
- * Get all (also childs) members of a group, the group items are excluded from this list.
- *
- * @param {*} group Name of the openHAB group
- * @param {*} characteristic Which characteristic of the members you get, valid: name (default), label, state
- */
-GroupUtils.prototype.getAllMembers = function (group, characteristic) {
-  this.log.debug('Getting all members of group ' + group)
-  var groupAllMembers = Array()
-  itemRegistry.getItem(group).getAllMembers().stream().forEach(function (item) {
-    if (characteristic === 'label') {
-      groupAllMembers.push(item.getLabel())
-    } else if (characteristic === 'state') {
-      groupAllMembers.push(item.getState())
-    } else {
-      groupAllMembers.push(item.getName())
-    }
-  })
-  // return the array
-  return groupAllMembers
-}
-
-/**
- * Perform arithmetic operations on multiple states.
- *
- * @param {*} items Array of states
- * @param {*} func Arithmetic function to perform, valid: SUM, AVG, MIN, MAX
- */
-GroupUtils.prototype.arithmetic = function (items, func) {
-  if (func === 'SUM') {
-    var sum = items[0]
-    for (var i = 1; i < items.length; i++) {
-      sum += items[i]
-    }
-    return sum
-  } else if (func === 'AVG') {
-    var sum = items[0]
-    for (var i = 1; i < items.length; i++) {
-      sum += items[i]
-    }
-    var avg = sum / items.length
-    return avg
-  } else if (func === 'MIN') {
-    var min = items[0]
-    for (var i = 1; i < items.length; ++i) {
-      if (items[i] < min) {
-        min = items[i]
-      }
-    }
-    return min
-  } else if (func === 'MAX') {
-    var max = items[0]
-    for (var i = 1; i < items.length; ++i) {
-      if (items[i] > max) {
-        max = items[i]
-      }
-    }
-    return max
+  /**
+   * Get the direct members of a group.
+   *
+   * @param {*} group Name of the openHAB group
+   * @param {*} characteristic Which characteristic of the members you get, valid: name (default), label, state
+   */
+  context.getMembers = function (group, characteristic) {
+    logger.debug('Getting direct members of group ' + group)
+    var groupMembers = context.itemRegistry.getItem(group) // an Java ArrayList
+      .getMembers()
+      .stream()
+      .map(function (i) {
+        if (characteristic === 'label') {
+          return i.getLabel()
+        } else if (characteristic === 'state') {
+          return i.getState()
+        } else {
+          return i.getName()
+        }
+      })
+      .collect(Collectors.toList())
+    return Java.from(groupMembers) // convert to JavaScript Array
   }
-}
 
-/**
- * Returns the number of states matching the given comparison expression.
- *
- * @param {*} items Array of states
- * @param {*} op Comparison operator, valid: equal, notEqual, larger, largerEqual, smaller, smallerEqual
- * @param {*} comp State to compare with
- */
-GroupUtils.prototype.count = function (items, op, comp) {
-  var counter = 0
-  for (var i = 0; i < items.length; ++i) {
-    if (op === 'equal') {
-      if (items[i] == comp) {
-        ++counter
+  /**
+   * Get all (also childs) members of a group.
+   *
+   * @param {*} group Name of the openHAB group
+   * @param {*} characteristic Which characteristic of the members you get, valid: name (default), label, state
+   */
+  context.getAllMembers = function (group, characteristic) {
+    logger.debug('Getting all members of group ' + group)
+    var groupAllMembers = context.itemRegistry.getItem(group) // an Java ArrayList
+      .getMembers()
+      .stream()
+      .map(function (i) {
+        if (characteristic === 'label') {
+          return i.getLabel()
+        } else if (characteristic === 'state') {
+          return i.getState()
+        } else {
+          return i.getName()
+        }
+      })
+      .collect(Collectors.toList())
+    return Java.from(groupAllMembers) // convert to JavaScript Array
+  }
+
+  /**
+   * Perform arithmetic operations on an given array of states.
+   *
+   * @param {*} items Array of states
+   * @param {*} func Arithmetic function to perform, valid: SUM, AVG, MIN, MAX
+   */
+  context.arithmetic = function (items, func) {
+    if (func === 'SUM') {
+      var sum = items[0]
+      for (var i = 1; i < items.length; i++) {
+        sum += items[i]
       }
-    } else if (op === 'notEqual') {
-      if (items[i] != comp) {
-        ++counter
+      return sum
+    } else if (func === 'AVG') {
+      var sum = items[0]
+      for (var i = 1; i < items.length; i++) {
+        sum += items[i]
       }
-    } else if (op === 'larger') {
-      if (items[i] > comp) {
-        ++counter
+      var avg = sum / items.length
+      return avg
+    } else if (func === 'MIN') {
+      var min = items[0]
+      for (var i = 1; i < items.length; ++i) {
+        if (items[i] < min) {
+          min = items[i]
+        }
       }
-    } else if (op === 'largerEqual') {
-      if (items[i] >= comp) {
-        ++counter
+      return min
+    } else if (func === 'MAX') {
+      var max = items[0]
+      for (var i = 1; i < items.length; ++i) {
+        if (items[i] > max) {
+          max = items[i]
+        }
       }
-    } else if (op === 'smaller') {
-      if (items[i] < comp) {
-        ++counter
-      }
-    } else if (op === 'smallerEqual') {
-      if (items[i] <= comp) {
-        ++counter
-      }
+      return max
     }
   }
-  return counter
-}
+
+  /**
+   * Returns the number of states matching the given operator and target.
+   *
+   * @param {*} items Array of states
+   * @param {*} op Operator, valid: equal, notEqual, larger, largerEqual, smaller, smallerEqual
+   * @param {*} comp State to compare with
+   */
+  context.count = function (items, op, comp) {
+    var counter = 0
+    for (var i = 0; i < items.length; ++i) {
+      if (op === 'equal') {
+        if (items[i] == comp) {
+          ++counter
+        }
+      } else if (op === 'notEqual') {
+        if (items[i] != comp) {
+          ++counter
+        }
+      } else if (op === 'larger') {
+        if (items[i] > comp) {
+          ++counter
+        }
+      } else if (op === 'largerEqual') {
+        if (items[i] >= comp) {
+          ++counter
+        }
+      } else if (op === 'smaller') {
+        if (items[i] < comp) {
+          ++counter
+        }
+      } else if (op === 'smallerEqual') {
+        if (items[i] <= comp) {
+          ++counter
+        }
+      }
+    }
+    return counter
+  }
+})(this)
