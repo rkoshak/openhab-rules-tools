8bd1793a0f4cbc3b5a0dd26cf5b5d94c503b74a1
Richard Koshak <rlkoshak@gmail.com>
2022-01-19 09:33:19 -0700

    Added comments back to rateLimit

--- rateLimit.js
+++ rateLimit.js
@@ -1,11 +1,23 @@
 const {timeUtils} = require('openhab_rules_tools');
 
+/**
+ * Simple class that keeps track of a time. When run is called, a 
+ * when is passed indicating how much time to wait after that call
+ * before calling the passed in func again.
+ */
 class RateLimit {
 
+  /**
+   * Initializes the delay to the past so the first call immediately runs
+   */
   constructor() {
     this.until = time.ZonedDateTime.now().minusSeconds(1);
   }
 
+  /**
+   * @param {function()} func action to run if it's been long enough
+   * @when {*} anything supported by timeUtils.toDateTime
+   */
   run(func, when) {
     if(time.ZonedDateTime.now().isAfter(this.until)) {
       this.until = timeUtils.toDateTime(when);
