ee75111e19ca6a6ab03d882880b05765c46cd3db
Richard Koshak <rlkoshak@gmail.com>
2023-02-07 15:05:28 -0700

    Added a group and metadata checking helper function used by rule templates to validate their config.

--- helpers.js
+++ helpers.js
@@ -23,6 +23,77 @@ const createTimer = (when, func, name, key) => {
   return actions.ScriptExecution.createTimer(name, timeout, func);
 };
 
+/**
+ * Utility function mostly used by rule templates to check the metadata and Group membership
+ * of the Items that drive a rule like Debounce or Time Based State Machine. An Item metadata
+ * namespace and Group is passed along with a function and usage text.
+ *
+ * The function validates that all Items that have the given namespace are members of grp,
+ * no members of grp lack the namespace metadata, and the validateFunc is called to validate
+ * the metadata on each individual Item. validateFunc should throw an exception with the
+ * reason why the Item failed validation when it fails. The usage text will be logged if one
+ * or more Items fail validations.
+ *
+ * @param {string} namespace the Item metadata namespace to check
+ * @param {string} grp name of the Group Item all the Items with namespace should belong to
+ * @param {function} validateFunc function called to validate the metadata is valid
+ * @param {string} usage text to log out when an Item fails validation
+ * @returns {boolean} true if validation passes, false if there's a problem
+ **/
+const checkGrpAndMetadata = (namespace, grp, validateFunc, usage) => {
+  let isGood = true;
+  let badItems = [];
+
+  // Get all the Item with NAMESPACE metadata
+  const allItems = items.getItems();
+  const filtered = allItems.filter(item => item.getMetadata(namespace));
+
+  // Check the metadata of members of the Group membership
+  filtered.forEach(item => {
+    try {
+      const cfg = validateFunc(item.name);
+      if (!item.groupNames.includes(grp)) {
+        console.warn(item.name + ' has ' + namespace + ' metadata but is not a member of ' + grp + '!');
+        isGood = false;
+        badItems.push(item.name);
+      }
+    }
+    catch (e) {
+      console.warn('Item ' + item.name + "'s configuration is invalid:\n" + e);
+      isGood = false;
+      badItems.push(item.name);
+    }
+  });
+
+  // Check Group membership
+  if (!items[grp]) {
+    console.warn('There is no ' + grp + ' Group!');
+    isGood = false;
+  }
+  else if (!items[grp].members) {
+    console.warn(grp + ' does not have any members!');
+    isGood = false;
+  }
+  else {
+    items[grp].members.filter(item => { !item.getMetadata(namespace) }).forEach(item => {
+      console.warn(item.name + ' is a member of ' + grp + ' but lacks ' + namespace + ' metadata!');
+      isGood = false;
+      badItems.push(item.name);
+    });
+  }
+
+  // Report the bad Items and print usage info
+  if (isGood) {
+    console.info('All ' + namespace + ' Items are configured correctly');
+  }
+  else {
+    if (badItems.length) console.warn('The following Items have an invalid configuration: ' + badItems + '\n');
+    console.warn(usage);
+  }
+  return isGood;
+};
+
 module.exports = {
-  createTimer
+  createTimer,
+  checkGrpAndMetadata
 };
