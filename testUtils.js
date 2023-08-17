ee0b143bb2bdfa794ac51eb64e78c97fb18938dd
Richard Koshak <rlkoshak@gmail.com>
2023-08-17 11:20:51 -0600

    Added comments, reimplemented sleep
    Signed-off-by: Richard Koshak <rlkoshak@gmail.com>

--- testUtils.js
+++ testUtils.js
@@ -1,19 +1,24 @@
 const { time } = require('openhab');
 
+/**
+ * If the condition is false, throw an exception with the message
+ * @param {boolean} condition if true, do nothing
+ * @param {string} message exception message if condition is false
+ */
 const assert = (condition, message) => {
   if (!condition) {
     throw new Error(message || 'Assertion failed');
   }
 }
 
+/**
+ * Simple wrapper around javba.lang.Thread.sleep()
+ *
+ * @param {int} msec number of milliseconds to sleep
+ */
 const sleep = (msec) => {
-  let curr = time.toZDT();
-  const done = curr.plus(msec, time.ChronoUnit.MILLIS);
-  const timeout = time.toZDT('PT5s');
-  while (curr.isBefore(done) && curr.isBefore(timeout)) { // busy wait
-    curr = time.toZDT();
-  }
-  if (curr.isAfter(timeout)) console.error('sleep timed out!');
+  var Thread = Java.type('java.lang.Thread');
+  Thread.sleep(msec);
 }
 
 module.exports = {
